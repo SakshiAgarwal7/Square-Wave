666d5fdc67b904469c81854270d7f51586c6b978
akhileshgupta1982 <69047835+akhileshgupta1982@users.noreply.github.com>
2020-09-09 21:10:39 +0530

    all files

---
+++ experiment/simulation/Vm_change.js
@@ -0,0 +1,15 @@
+function Vchange(){
+   Vm=document.getElementById("Vp").value;
+document.getElementById("Voltage_amplitude").value=Vm;
+
+}
+function HO(){
+   hor=document.getElementById("HA").value;
+    document.getElementById("HAN").value=hor;
+	
+}
+function Frq(){
+   fr=document.getElementById("fo").value;
+    document.getElementById("Frequency").value=fr;
+	
+}
